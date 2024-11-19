be4eb2c65ceed489f3e91054cfb8ac0e44df4e29
Lukáš Kvídera <kvideral@qwsome.eu>
2024-11-19 09:02:31 +0100

    #1209 Fixed MMAIndicatorTest

--- ta4j-core/src/test/java/org/ta4j/core/indicators/numeric/average/MMAIndicatorTest.java
+++ ta4j-core/src/test/java/org/ta4j/core/indicators/numeric/average/MMAIndicatorTest.java
@@ -2,6 +2,8 @@
  * The MIT License (MIT)
  *
  * Copyright (c) 2024 Lukáš Kvídera
+ * Copyright (c) 2017-2024 Ta4j Organization & respective
+ * authors (see AUTHORS)
  *
  * Permission is hereby granted, free of charge, to any person obtaining a copy of
  * this software and associated documentation files (the "Software"), to deal in
@@ -22,95 +24,98 @@
  */
 package org.ta4j.core.indicators.numeric.average;
 
-import static org.junit.Assert.assertEquals;
-import static org.ta4j.core.TestUtils.assertNext;
-import static org.ta4j.core.TestUtils.fastForward;
-
-import org.junit.Before;
-import org.junit.Test;
-import org.ta4j.core.ExternalIndicatorTest;
-import org.ta4j.core.MockStrategy;
-import org.ta4j.core.TestIndicator;
-import org.ta4j.core.TestUtils;
-import org.ta4j.core.backtest.BacktestBarSeries;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.MethodSource;
+import org.ta4j.core.TestContext;
 import org.ta4j.core.indicators.AbstractIndicatorTest;
 import org.ta4j.core.indicators.XLSIndicatorTest;
-import org.ta4j.core.indicators.numeric.NumericIndicator;
-import org.ta4j.core.mocks.MockBarSeriesBuilder;
+import org.ta4j.core.indicators.numeric.Indicators;
 import org.ta4j.core.num.Num;
 import org.ta4j.core.num.NumFactory;
 
-public class MMAIndicatorTest extends AbstractIndicatorTest<Num> {
-
-  private final ExternalIndicatorTest xls;
+class MMAIndicatorTest extends AbstractIndicatorTest<Num> {
 
 
-  public MMAIndicatorTest(final NumFactory numFunction) {
-    super(numFunction);
-    this.xls = new XLSIndicatorTest(this.getClass(), "MMA.xls", 6, numFunction);
+  private TestContext testContext;
+
+
+  @BeforeEach
+  void setUp() {
+    this.testContext = new TestContext();
+    this.testContext.withCandlePrices(
+        64.75,
+        63.79,
+        63.73,
+        63.73,
+        63.55,
+        63.19,
+        63.91,
+        63.85,
+        62.95,
+        63.37,
+        61.33,
+        61.51
+    );
   }
 
 
-  private BacktestBarSeries data;
+  @ParameterizedTest(name = "MMA 1 [{index}] {0}")
+  @MethodSource("provideNumFactories")
+  void firstValueShouldBeEqualsToFirstDataValue(final NumFactory numFactory) {
+    this.testContext.withNumFactory(numFactory);
 
+    final var actualIndicator = Indicators.closePrice().mma(1);
 
-  @Before
-  public void setUp() {
-    this.data = new MockBarSeriesBuilder().withNumFactory(this.numFactory)
-        .withData(64.75, 63.79, 63.73, 63.73, 63.55, 63.19, 63.91, 63.85, 62.95, 63.37, 61.33, 61.51)
-        .build();
+    this.testContext.withIndicator(actualIndicator)
+        .assertNext(64.75);
   }
 
 
-  @Test
-  public void firstValueShouldBeEqualsToFirstDataValue() {
-    final var actualIndicator = NumericIndicator.closePrice(this.data).mma(1);
-    this.data.replaceStrategy(new MockStrategy(actualIndicator));
-    this.data.advance();
-    assertEquals(64.75, actualIndicator.getValue().doubleValue(), TestUtils.GENERAL_OFFSET);
+  @ParameterizedTest(name = "MMA 1 [{index}] {0}")
+  @MethodSource("provideNumFactories")
+  void mmaUsingBarCount10UsingClosePrice(final NumFactory numFactory) {
+    this.testContext.withNumFactory(numFactory);
+
+    final var actualIndicator = Indicators.closePrice().mma(10);
+
+    this.testContext.withIndicator(actualIndicator)
+        .fastForwardUntilStable()
+        .assertCurrent(63.9983)
+        .assertNext(63.7315)
+        .assertNext(63.5093)
+    ;
   }
 
 
-  @Test
-  public void mmaUsingBarCount10UsingClosePrice() {
-    final var actualIndicator = NumericIndicator.closePrice(this.data).mma(10);
-    this.data.replaceStrategy(new MockStrategy(actualIndicator));
-
-
-    fastForward(this.data, 10);
-
-    assertNext(this.data, 63.9983, actualIndicator);
-    assertNext(this.data, 63.7315, actualIndicator);
-    assertNext(this.data, 63.5093, actualIndicator);
+  @ParameterizedTest(name = "MMA 1 [{index}] {0}")
+  @MethodSource("provideNumFactories")
+  void testAgainstExternalData1(final NumFactory numFactory) throws Exception {
+    assertBarCount(1, 329.0, numFactory);
   }
 
 
-  @Test
-  public void testAgainstExternalData1() throws Exception {
-    assertBarCount(1, 329.0);
+  @ParameterizedTest(name = "MMA 3 [{index}] {0}")
+  @MethodSource("provideNumFactories")
+  void testAgainstExternalData3(final NumFactory numFactory) throws Exception {
+    assertBarCount(3, 327.2900, numFactory);
   }
 
 
-  @Test
-  public void testAgainstExternalData3() throws Exception {
-    assertBarCount(3, 327.2900);
+  @ParameterizedTest(name = "MMA 13 [{index}] {0}")
+  @MethodSource("provideNumFactories")
+  void testAgainstExternalData13(final NumFactory numFactory) throws Exception {
+    assertBarCount(13, 326.9696, numFactory);
   }
 
 
-  @Test
-  public void testAgainstExternalData13() throws Exception {
-    assertBarCount(13, 326.9696);
+  private void assertBarCount(final int barCount, final double expected, final NumFactory numFactory) throws Exception {
+    final var xlsContext = new TestContext();
+    final var xls = new XLSIndicatorTest(this.getClass(), "MMA.xls", 6, numFactory);
+    xlsContext.withMarketEvents(xls.getMarketEvents());
+    final var actualIndicator = Indicators.closePrice().mma(barCount);
+    final var expectedIndicator = xls.getIndicator(barCount);
+    xlsContext.withIndicators(actualIndicator, expectedIndicator)
+        .assertIndicatorEquals(expectedIndicator, actualIndicator);
   }
-
-
-  private void assertBarCount(final int barCount, final double expected) throws Exception {
-    final var xlsSeries = this.xls.getMarketEvents();
-    final var actualIndicator = NumericIndicator.closePrice(xlsSeries).mma(barCount);
-    final var expectedIndicator = this.xls.getIndicator(barCount);
-    xlsSeries.replaceStrategy(new MockStrategy(actualIndicator, expectedIndicator));
-
-    assertIndicatorEquals(expectedIndicator, new TestIndicator<>(xlsSeries, actualIndicator));
-    assertEquals(expected, actualIndicator.getValue().doubleValue(), TestUtils.GENERAL_OFFSET);
-  }
-
 }
