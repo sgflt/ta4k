eb1ea52349397e58432c08adf705acfee24aaf4b
Lukáš Kvídera <kvideral@qwsome.eu>
2024-12-08 15:04:34 +0100

    #1209 Enabled NumberOfBreakEvenPositionsCriterionTest

--- ta4j-core/src/test/java/org/ta4j/core/criteria/NumberOfBreakEvenPositionsCriterionTest.java
+++ ta4j-core/src/test/java/org/ta4j/core/criteria/NumberOfBreakEvenPositionsCriterionTest.java
@@ -23,83 +23,60 @@
  */
 package org.ta4j.core.criteria;
 
-import static org.junit.Assert.assertFalse;
-import static org.junit.Assert.assertTrue;
-import static org.ta4j.core.TestUtils.assertNumEquals;
-
-import org.junit.Test;
-import org.ta4j.core.AnalysisCriterion;
-import org.ta4j.core.Position;
+import org.junit.jupiter.api.Test;
 import org.ta4j.core.Trade;
-import org.ta4j.core.TradingRecord;
-import org.ta4j.core.backtest.BackTestTradingRecord;
-import org.ta4j.core.mocks.MockBarSeriesBuilder;
-import org.ta4j.core.num.NumFactory;
+import org.ta4j.core.TradingRecordTestContext;
 
-public class NumberOfBreakEvenPositionsCriterionTest extends AbstractCriterionTest {
+class NumberOfBreakEvenPositionsCriterionTest {
 
-  public NumberOfBreakEvenPositionsCriterionTest(final NumFactory numFactory) {
-    super(params -> new NumberOfBreakEvenPositionsCriterion(), numFactory);
+  @Test
+  void calculateWithNoPositions() {
+    new TradingRecordTestContext()
+        .withCriterion(new NumberOfBreakEvenPositionsCriterion())
+        .assertResults(0);
   }
 
 
   @Test
-  public void calculateWithNoPositions() {
-    final var series = new MockBarSeriesBuilder().withNumFactory(this.numFactory)
-        .withData(100, 105, 110, 100, 95, 105)
-        .build();
-
-    assertNumEquals(0, getCriterion().calculate(series, new BackTestTradingRecord()));
+  void calculateWithTwoLongPositions() {
+    new TradingRecordTestContext()
+        .withCriterion(new NumberOfBreakEvenPositionsCriterion())
+        .enter(1).at(100)
+        .exit(1).at(100)
+        .enter(1).at(105)
+        .exit(1).at(105)
+        .assertResults(2);
   }
 
 
   @Test
-  public void calculateWithTwoLongPositions() {
-    final var series = new MockBarSeriesBuilder().withNumFactory(this.numFactory)
-        .withData(100, 105, 110, 100, 95, 105)
-        .build();
-    final TradingRecord tradingRecord = new BackTestTradingRecord(Trade.buyAt(0, series), Trade.sellAt(3, series),
-        Trade.buyAt(1, series), Trade.sellAt(5, series)
-    );
-
-    assertNumEquals(2, getCriterion().calculate(series, tradingRecord));
+  void calculateWithOneLongPosition() {
+    new TradingRecordTestContext()
+        .withCriterion(new NumberOfBreakEvenPositionsCriterion())
+        .enter(1).at(100)
+        .exit(1).at(100)
+        .assertResults(1);
   }
 
 
   @Test
-  public void calculateWithOneLongPosition() {
-    final var series = new MockBarSeriesBuilder().withNumFactory(this.numFactory)
-        .withData(100, 105, 110, 100, 95, 105)
-        .build();
-    final Position position = new Position(Trade.buyAt(0, series), Trade.sellAt(3, series));
-
-    assertNumEquals(1, getCriterion().calculate(series, position));
+  void calculateWithTwoShortPositions() {
+    new TradingRecordTestContext()
+        .withTradeType(Trade.TradeType.SELL)
+        .withCriterion(new NumberOfBreakEvenPositionsCriterion())
+        .enter(1).at(100)
+        .exit(1).at(100)
+        .enter(1).at(105)
+        .exit(1).at(105)
+        .assertResults(2);
   }
 
 
   @Test
-  public void calculateWithTwoShortPositions() {
-    final var series = new MockBarSeriesBuilder().withNumFactory(this.numFactory)
-        .withData(100, 105, 110, 100, 95, 105)
-        .build();
-    final TradingRecord tradingRecord = new BackTestTradingRecord(Trade.sellAt(0, series), Trade.buyAt(3, series),
-        Trade.sellAt(1, series), Trade.buyAt(5, series)
-    );
-
-    assertNumEquals(2, getCriterion().calculate(series, tradingRecord));
-  }
-
-
-  @Test
-  public void betterThan() {
-    final AnalysisCriterion criterion = getCriterion();
-    assertTrue(criterion.betterThan(numOf(3), numOf(6)));
-    assertFalse(criterion.betterThan(numOf(7), numOf(4)));
-  }
-
-
-  @Test
-  public void testCalculateOneOpenPositionShouldReturnZero() {
-    this.openedPositionUtils.testCalculateOneOpenPositionShouldReturnExpectedValue(this.numFactory, getCriterion(), 0);
+  void calculateOneOpenPositionShouldReturnZero() {
+    new TradingRecordTestContext()
+        .withCriterion(new NumberOfBreakEvenPositionsCriterion())
+        .enter(1).at(100)
+        .assertResults(0);
   }
 }
