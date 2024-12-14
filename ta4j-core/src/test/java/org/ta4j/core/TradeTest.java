81233f896038a59dbc60e98f10380c0a254a130d
Lukáš Kvídera <kvideral@qwsome.eu>
2024-12-14 12:12:20 +0100

    #1209 Enabled TradeTest

--- ta4j-core/src/test/java/org/ta4j/core/TradeTest.java
+++ ta4j-core/src/test/java/org/ta4j/core/TradeTest.java
@@ -1,97 +1,122 @@
-///**
-// * The MIT License (MIT)
-// *
-// * Copyright (c) 2017-2023 Ta4j Organization & respective
-// * authors (see AUTHORS)
-// *
-// * Permission is hereby granted, free of charge, to any person obtaining a copy of
-// * this software and associated documentation files (the "Software"), to deal in
-// * the Software without restriction, including without limitation the rights to
-// * use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of
-// * the Software, and to permit persons to whom the Software is furnished to do so,
-// * subject to the following conditions:
-// *
-// * The above copyright notice and this permission notice shall be included in all
-// * copies or substantial portions of the Software.
-// *
-// * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-// * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS
-// * FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR
-// * COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER
-// * IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
-// * CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
-// */
-//package org.ta4j.core;
-//
-//import static org.junit.Assert.assertEquals;
-//import static org.junit.Assert.assertFalse;
-//import static org.junit.Assert.assertNotEquals;
-//import static org.junit.Assert.assertTrue;
-//import static org.ta4j.core.TestUtils.assertNumEquals;
-//import static org.ta4j.core.num.NaN.NaN;
-//
-//import org.junit.Before;
-//import org.junit.Test;
-//import org.ta4j.core.Trade.TradeType;
-//import org.ta4j.core.analysis.cost.LinearTransactionCostModel;
-//import org.ta4j.core.mocks.MockBarSeriesBuilder;
-//import org.ta4j.core.num.DoubleNum;
-//import org.ta4j.core.num.DoubleNumFactory;
-//import org.ta4j.core.num.Num;
-//
-//public class TradeTest {
-//
-//    Trade opEquals1, opEquals2, opNotEquals1, opNotEquals2;
-//
-//    @Before
-//    public void setUp() {
-//        opEquals1 = Trade.buyAt(1, NaN, NaN);
-//        opEquals2 = Trade.buyAt(1, NaN, NaN);
-//
-//        opNotEquals1 = Trade.sellAt(1, NaN, NaN);
-//        opNotEquals2 = Trade.buyAt(2, NaN, NaN);
-//    }
-//
-//    @Test
-//    public void type() {
-//        assertEquals(TradeType.SELL, opNotEquals1.getType());
-//        assertFalse(opNotEquals1.isBuy());
-//        assertTrue(opNotEquals1.isSell());
-//        assertEquals(TradeType.BUY, opNotEquals2.getType());
-//        assertTrue(opNotEquals2.isBuy());
-//        assertFalse(opNotEquals2.isSell());
-//    }
-//
-//    @Test
-//    public void overrideToString() {
-//        assertEquals(opEquals1.toString(), opEquals2.toString());
-//
-//        assertNotEquals(opEquals1.toString(), opNotEquals1.toString());
-//        assertNotEquals(opEquals1.toString(), opNotEquals2.toString());
-//    }
-//
-//    @Test
-//    public void initializeWithCostsTest() {
-//        var transactionCostModel = new LinearTransactionCostModel(0.05);
-//        var trade = new Trade(0, TradeType.BUY, DoubleNum.valueOf(100), DoubleNum.valueOf(20), transactionCostModel);
-//        Num expectedCost = DoubleNum.valueOf(100);
-//        Num expectedValue = DoubleNum.valueOf(2000);
-//        Num expectedRawPrice = DoubleNum.valueOf(100);
-//        Num expectedNetPrice = DoubleNum.valueOf(105);
-//
-//        assertNumEquals(expectedCost, trade.getCost());
-//        assertNumEquals(expectedValue, trade.getValue());
-//        assertNumEquals(expectedRawPrice, trade.getPricePerAsset());
-//        assertNumEquals(expectedNetPrice, trade.getNetPrice());
-//        assertTrue(transactionCostModel.equals(trade.getCostModel()));
-//    }
-//
-//    @Test
-//    public void testReturnBarSeriesCloseOnNaN() {
-//        var series = new MockBarSeriesBuilder().withNumFactory(DoubleNumFactory.getInstance())
-//                .withCandleClosePrices(100, 95, 100, 80, 85, 130)
-//                .build();
-//        Trade trade = new Trade(1, TradeType.BUY, NaN);
-//        assertNumEquals(DoubleNum.valueOf(95), trade.getPricePerAsset(series));
-//    }
-//}
+/*
+ * The MIT License (MIT)
+ *
+ * Copyright (c) 2017-2024 Ta4j Organization & respective
+ * authors (see AUTHORS)
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy of
+ * this software and associated documentation files (the "Software"), to deal in
+ * the Software without restriction, including without limitation the rights to
+ * use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of
+ * the Software, and to permit persons to whom the Software is furnished to do so,
+ * subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS
+ * FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR
+ * COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER
+ * IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
+ * CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ */
+package org.ta4j.core;
+
+import static org.ta4j.core.TestUtils.assertNumEquals;
+
+import java.time.Instant;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.MethodSource;
+import org.ta4j.core.Trade.OrderType;
+import org.ta4j.core.Trade.TradeType;
+import org.ta4j.core.analysis.cost.LinearTransactionCostModel;
+import org.ta4j.core.analysis.cost.ZeroCostModel;
+import org.ta4j.core.num.DoubleNum;
+import org.ta4j.core.num.NumFactory;
+
+class TradeTest {
+
+  @Test
+  void buyTransactionShouldIncreaseNetPriceByTransactionCosts() {
+    final var transactionCostModel = new LinearTransactionCostModel(0.1); // 10% fee
+    final var trade = Trade.builder()
+        .type(TradeType.BUY)
+        .orderType(OrderType.OPEN)
+        .whenExecuted(Instant.EPOCH)
+        .pricePerAsset(DoubleNum.valueOf(100))
+        .amount(DoubleNum.valueOf(1))
+        .transactionCostModel(transactionCostModel)
+        .build();
+
+    assertNumEquals(100, trade.getPricePerAsset()); // Original price
+    assertNumEquals(110, trade.getNetPrice()); // Price + 10% fee
+    assertNumEquals(10, trade.getCost()); // Transaction cost
+  }
+
+
+  @Test
+  void sellTransactionShouldDecreaseNetPriceByTransactionCosts() {
+    final var transactionCostModel = new LinearTransactionCostModel(0.1); // 10% fee
+    final var trade = Trade.builder()
+        .type(TradeType.SELL)
+        .orderType(OrderType.CLOSE)
+        .whenExecuted(Instant.EPOCH)
+        .pricePerAsset(DoubleNum.valueOf(100))
+        .amount(DoubleNum.valueOf(1))
+        .transactionCostModel(transactionCostModel)
+        .build();
+
+    assertNumEquals(100, trade.getPricePerAsset()); // Original price
+    assertNumEquals(90, trade.getNetPrice()); // Price - 10% fee
+    assertNumEquals(10, trade.getCost()); // Transaction cost
+  }
+
+
+  @Test
+  void tradingValueShouldBeBasedOnPriceAndAmount() {
+    final var trade = Trade.builder()
+        .type(TradeType.BUY)
+        .transactionCostModel(new ZeroCostModel())
+        .orderType(OrderType.OPEN)
+        .whenExecuted(Instant.EPOCH)
+        .pricePerAsset(DoubleNum.valueOf(100))
+        .amount(DoubleNum.valueOf(2))
+        .build();
+
+    // Value = price * amount (without transaction costs)
+    assertNumEquals(200, trade.getValue());
+  }
+
+
+  @ParameterizedTest
+  @MethodSource("org.ta4j.core.NumFactoryTestSource#numFactories")
+  void costsShouldScaleWithTradeSize(final NumFactory numFactory) {
+    final var transactionCostModel = new LinearTransactionCostModel(0.01); // 1% fee
+
+    // Small trade
+    final var smallTrade = Trade.builder()
+        .type(TradeType.BUY)
+        .orderType(OrderType.OPEN)
+        .whenExecuted(Instant.EPOCH)
+        .pricePerAsset(numFactory.numOf(100))
+        .amount(numFactory.numOf(1))
+        .transactionCostModel(transactionCostModel)
+        .build();
+
+    // Large trade - 10x size
+    final var largeTrade = Trade.builder()
+        .type(TradeType.BUY)
+        .orderType(OrderType.OPEN)
+        .whenExecuted(Instant.EPOCH)
+        .pricePerAsset(numFactory.numOf(100))
+        .amount(numFactory.numOf(10))
+        .transactionCostModel(transactionCostModel)
+        .build();
+
+    // Cost should scale linearly with amount
+    assertNumEquals(largeTrade.getCost(), smallTrade.getCost().multipliedBy(numFactory.numOf(10)));
+  }
+}
