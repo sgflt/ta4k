46cd6bba9bf2e86780e3af0512c477e3e91ed36d
Lukáš Kvídera <kvideral@qwsome.eu>
2025-02-11 16:56:50 +0100

    #1209 Enabled StochasticOscillatorDIndicator

--- ta4j-core/src/test/java/org/ta4j/core/indicators/numeric/oscillators/StochasticOscillatorDIndicatorTest.java
+++ ta4j-core/src/test/java/org/ta4j/core/indicators/numeric/oscillators/StochasticOscillatorDIndicatorTest.java
@@ -1,88 +1,132 @@
-///**
-// * The MIT License (MIT)
-// *
-// * Copyright (c) 2017-2023 Ta4j Organization & respective
-// * authors (see AUTHORS)
-// *
-// * Permission is hereby granted, free of charge, to any person obtaining a copy of
-// * this software and associated documentation files (the "Software"), to deal in
-// * the Software without restriction, including without limitation the rights to
-// * use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of
-// * the Software, and to permit persons to whom the Software is furnished to do so,
-// * subject to the following conditions:
-// *
-// * The above copyright notice and this permission notice shall be included in all
-// * copies or substantial portions of the Software.
-// *
-// * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-// * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS
-// * FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR
-// * COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER
-// * IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
-// * CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
-// */
-//package org.ta4j.core.indicators;
-//
-//import static junit.framework.TestCase.assertEquals;
-//
-//import org.junit.Before;
-//import org.junit.Test;
-//import org.ta4j.core.api.bar.BarSeries;
-//import org.ta4j.core.indicators.numeric.average.SMAIndicator;
-//import org.ta4j.core.mocks.MockBarSeriesBuilder;
-//import org.ta4j.core.num.Num;
-//import org.ta4j.core.num.NumFactory;
-//
-//public class StochasticOscillatorDIndicatorTest extends AbstractIndicatorTest<Indicator<Num>, Num> {
-//    private BarSeries data;
-//
-//    public StochasticOscillatorDIndicatorTest(NumFactory numFactory) {
-//        super(numFactory);
-//    }
-//
-//    @Before
-//    public void setUp() {
-//
-//        data = new MockBarSeriesBuilder().withNumFactory(numFactory).build();
-//        data.barBuilder().openPrice(44.98).closePrice(119.13).highPrice(119.50).lowPrice(116.00).onCandle();
-//        data.barBuilder().openPrice(45.05).closePrice(116.75).highPrice(119.94).lowPrice(116.00).onCandle();
-//        data.barBuilder().openPrice(45.11).closePrice(113.50).highPrice(118.44).lowPrice(111.63).onCandle();
-//        data.barBuilder().openPrice(45.19).closePrice(111.56).highPrice(114.19).lowPrice(110.06).onCandle();
-//        data.barBuilder().openPrice(45.12).closePrice(112.25).highPrice(112.81).lowPrice(109.63).onCandle();
-//        data.barBuilder().openPrice(45.15).closePrice(110.00).highPrice(113.44).lowPrice(109.13).onCandle();
-//        data.barBuilder().openPrice(45.13).closePrice(113.50).highPrice(115.81).lowPrice(110.38).onCandle();
-//        data.barBuilder().openPrice(45.12).closePrice(117.13).highPrice(117.50).lowPrice(114.06).onCandle();
-//        data.barBuilder().openPrice(45.15).closePrice(115.63).highPrice(118.44).lowPrice(114.81).onCandle();
-//        data.barBuilder().openPrice(45.24).closePrice(114.13).highPrice(116.88).lowPrice(113.13).onCandle();
-//        data.barBuilder().openPrice(45.43).closePrice(118.81).highPrice(119.00).lowPrice(116.19).onCandle();
-//        data.barBuilder().openPrice(45.43).closePrice(117.38).highPrice(119.75).lowPrice(117.00).onCandle();
-//        data.barBuilder().openPrice(45.58).closePrice(119.13).highPrice(119.13).lowPrice(116.88).onCandle();
-//        data.barBuilder().openPrice(45.58).closePrice(115.38).highPrice(119.44).lowPrice(114.56).onCandle();
-//
-//    }
-//
-//    @Test
-//    public void stochasticOscilatorDParam14UsingSMA3AndGenericConstructer() {
-//
-//        var sof = new StochasticOscillatorKIndicator(data, 14);
-//        var sma = new SMAIndicator(sof, 3);
-//        var sos = new StochasticOscillatorDIndicator(sma);
-//
-//        assertEquals(sma.getValue(0), sos.getValue(0));
-//        assertEquals(sma.getValue(1), sos.getValue(1));
-//        assertEquals(sma.getValue(2), sos.getValue(2));
-//    }
-//
-//    @Test
-//    public void stochasticOscilatorDParam14UsingSMA3() {
-//
-//        var sof = new StochasticOscillatorKIndicator(data, 14);
-//        var sos = new StochasticOscillatorDIndicator(sof);
-//        var sma = new SMAIndicator(sof, 3);
-//
-//        assertEquals(sma.getValue(0), sos.getValue(0));
-//        assertEquals(sma.getValue(1), sos.getValue(1));
-//        assertEquals(sma.getValue(2), sos.getValue(2));
-//        assertEquals(sma.getValue(13), sos.getValue(13));
-//    }
-//}
+/*
+ * The MIT License (MIT)
+ *
+ * Copyright (c) 2017-2025 Ta4j Organization & respective
+ * authors (see AUTHORS)
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy of
+ * this software and associated documentation files (the "Software"), to deal in
+ * the Software without restriction, including without limitation the rights to
+ * use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of
+ * the Software, and to permit persons to whom the Software is furnished to do so,
+ * subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS
+ * FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR
+ * COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER
+ * IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
+ * CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ */
+package org.ta4j.core.indicators.numeric.oscillators;
+
+import java.time.Instant;
+import java.time.temporal.ChronoUnit;
+import java.util.List;
+
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.MethodSource;
+import org.ta4j.core.MarketEventTestContext;
+import org.ta4j.core.events.CandleReceived;
+import org.ta4j.core.indicators.TimeFrame;
+import org.ta4j.core.indicators.numeric.oscilators.StochasticOscillatorDIndicator;
+import org.ta4j.core.indicators.numeric.oscilators.StochasticOscillatorKIndicator;
+import org.ta4j.core.num.NumFactory;
+
+class StochasticOscillatorDIndicatorTest {
+
+  @ParameterizedTest
+  @MethodSource("org.ta4j.core.NumFactoryTestSource#numFactories")
+  void noMovement(final NumFactory numFactory) {
+    final var context = new MarketEventTestContext()
+        .withNumFactory(numFactory)
+        .withCandlePrices(
+            1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 8, 7, 6, 5, 4, 3, 2, 1, 2, 3, 4, 5
+        );
+
+    final var k = new StochasticOscillatorKIndicator(numFactory, 2);
+    final var d = new StochasticOscillatorDIndicator(numFactory, k);
+
+    context.withIndicator(d);
+
+    context.fastForwardUntilStable()
+        // (C - L) / (H - L) => 0/0 => 100.0
+        .assertNext(100.0)
+        .assertNext(100.0)
+        .assertNext(100.0);
+  }
+
+
+  @ParameterizedTest
+  @MethodSource("org.ta4j.core.NumFactoryTestSource#numFactories")
+  void movement(final NumFactory numFactory) {
+    final var context = new MarketEventTestContext()
+        .withNumFactory(numFactory)
+        .withCandleDuration(ChronoUnit.MINUTES)
+        .withMarketEvents(
+            List.of(
+                CandleReceived.builder()
+                    .timeFrame(TimeFrame.DAY)
+                    .beginTime(Instant.EPOCH)
+                    .endTime(Instant.EPOCH.plusSeconds(60))
+                    .closePrice(1)
+                    .highPrice(2)
+                    .lowPrice(0)
+                    .build(),
+                CandleReceived.builder()
+                    .timeFrame(TimeFrame.DAY)
+                    .beginTime(Instant.EPOCH.plusSeconds(60))
+                    .endTime(Instant.EPOCH.plusSeconds(120))
+                    .closePrice(2)
+                    .highPrice(2)
+                    .lowPrice(0)
+                    .build(),
+                CandleReceived.builder()
+                    .timeFrame(TimeFrame.DAY)
+                    .beginTime(Instant.EPOCH.plusSeconds(120))
+                    .endTime(Instant.EPOCH.plusSeconds(180))
+                    .closePrice(3)
+                    .highPrice(3)
+                    .lowPrice(0)
+                    .build(),
+                CandleReceived.builder()
+                    .timeFrame(TimeFrame.DAY)
+                    .beginTime(Instant.EPOCH.plusSeconds(180))
+                    .endTime(Instant.EPOCH.plusSeconds(240))
+                    .closePrice(1)
+                    .highPrice(2)
+                    .lowPrice(0)
+                    .build()  ,
+                CandleReceived.builder()
+                    .timeFrame(TimeFrame.DAY)
+                    .beginTime(Instant.EPOCH.plusSeconds(240))
+                    .endTime(Instant.EPOCH.plusSeconds(300))
+                    .closePrice(2)
+                    .highPrice(12)
+                    .lowPrice(0)
+                    .build(),
+                CandleReceived.builder()
+                    .timeFrame(TimeFrame.DAY)
+                    .beginTime(Instant.EPOCH.plusSeconds(300))
+                    .endTime(Instant.EPOCH.plusSeconds(360))
+                    .closePrice(2)
+                    .highPrice(25)
+                    .lowPrice(1)
+                    .build()
+            )
+        );
+
+    final var k = new StochasticOscillatorKIndicator(numFactory, 2);
+    final var d = new StochasticOscillatorDIndicator(numFactory, k);
+
+    context.withIndicator(d);
+
+    context.fastForwardUntilStable()
+        .assertNext(77.7777)
+        .assertNext(49.9999)
+        .assertNext(19.3333);
+  }
+}
