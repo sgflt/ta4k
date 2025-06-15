a060db922f3f9866e2f0ab30463f1b2f4c7bc935
Lukáš Kvídera <kvideral@qwsome.eu>
2025-06-15 19:18:50 +0200

    Fixed package of LinearTransactionCostCriterionTest

--- ta4j-core/src/test/java/org/ta4j/core/backtest/criteria/LinearTransactionCostCriterionTest.kt
+++ ta4j-core/src/test/java/org/ta4j/core/backtest/criteria/LinearTransactionCostCriterionTest.kt
@@ -1,9 +1,34 @@
-package org.ta4j.core.criteria
+/*
+ * The MIT License (MIT)
+ *
+ * Copyright (c) 2017-2024 Ta4j Organization & respective authors (see AUTHORS)
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy of
+ * this software and associated documentation files (the "Software"), to deal in
+ * the Software without restriction, including without limitation the rights to
+ * use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of
+ * the Software, and to permit persons to whom the Software is furnished to do so,
+ * subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS
+ * FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR
+ * COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER
+ * IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
+ * CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ */
 
+package org.ta4j.core.backtest.criteria
+
+import org.junit.jupiter.api.Assumptions
 import org.junit.jupiter.params.ParameterizedTest
 import org.junit.jupiter.params.provider.MethodSource
 import org.ta4j.core.MarketEventTestContext
-import org.ta4j.core.backtest.criteria.LinearTransactionCostCriterion
+import org.ta4j.core.TestUtils
+import org.ta4j.core.criteria.XLSCriterionTest
 import org.ta4j.core.num.NumFactory
 
 class LinearTransactionCostCriterionTest {
@@ -14,9 +39,9 @@ class LinearTransactionCostCriterionTest {
         val xls = XLSCriterionTest(this.javaClass, "LTC.xls", 16, 6, numFactory)
         val tradingRecord = xls.tradingRecord
 
-        // Skip test if XLS file doesn't contain trading data  
+        // Skip test if XLS file doesn't contain trading data
         if (tradingRecord.positions.isEmpty()) {
-            org.junit.jupiter.api.Assumptions.assumeFalse(
+            Assumptions.assumeFalse(
                 tradingRecord.positions.isEmpty(),
                 "Skipping external data test - XLS file contains no trading positions"
             )
@@ -25,12 +50,12 @@ class LinearTransactionCostCriterionTest {
         // Test with first set of parameters
         val criterion1 = LinearTransactionCostCriterion(1000.0, 0.005, 0.2)
         val result1 = criterion1.calculate(tradingRecord)
-        org.ta4j.core.TestUtils.assertNumEquals(843.5492, result1)
+        TestUtils.assertNumEquals(843.5492, result1)
 
         // Test with second set of parameters
         val criterion2 = LinearTransactionCostCriterion(1000.0, 0.1, 1.0)
         val result2 = criterion2.calculate(tradingRecord)
-        org.ta4j.core.TestUtils.assertNumEquals(1122.4410, result2)
+        TestUtils.assertNumEquals(1122.4410, result2)
     }
 
     @ParameterizedTest
